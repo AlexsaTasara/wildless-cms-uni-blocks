369abe9a9e0f6001914a5da7b445d575d31658c4
Andrey Simonenko <a.dev.krd@gmail.com>
2022-07-29 15:50:35 +0300

    add isHrefSameOrigin

--- src/services/sitemap/isTopItemActive.ts
+++ src/services/sitemap/isTopItemActive.ts
@@ -1,9 +1,12 @@
 import type { Router } from '../../components/ContentPage/ContentPageContext';
 import { isHrefActive } from './isHrefActive';
+import { isHrefSameOrigin } from './isHrefSameOrigin';
 import { isSubItemActive } from './isSubItemActive';
 import { TopMenuItem } from './SitemapProps';
 
 export const isTopItemActive =
   (router: Router) =>
   (topItem: TopMenuItem): boolean =>
-    topItem.items?.some(isSubItemActive(router)) || isHrefActive(topItem.href, router);
+    topItem.items?.some(isSubItemActive(router)) ||
+    isHrefActive(topItem.href, router) ||
+    isHrefSameOrigin(topItem.href, router);

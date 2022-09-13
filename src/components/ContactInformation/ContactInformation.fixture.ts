370bdcb286118c663334980fb31a74b778e9db4e
AlexsaTasara <kirin1337@gmail.com>
2022-09-13 13:03:45 +0300

    draft Contact Information (#395)

---
+++ src/components/ContactInformation/ContactInformation.fixture.ts
@@ -0,0 +1,60 @@
+import { context } from '../../setup-fixture';
+import { ContactInformation } from './ContactInformation';
+import type { ContactData, ContactInformationContent, OneCard } from './ContactInformationContent';
+
+export const DATA1: ContactData = {
+  label: 'Телефон филиала',
+  value: ['8(495)200-78-70'],
+};
+
+export const DATA2: ContactData = {
+  label: 'Операционный зал',
+  value: ['8(495)200-78-70'],
+};
+
+export const DATA3: ContactData = {
+  label: 'Для консультации по контролю валюты',
+  value: ['8(800)200-78-70', '8(495)773-20-19'],
+};
+
+export const DATA4: ContactData = {
+  label: 'Факс',
+  value: ['8(800)200-78-70'],
+};
+
+export const DATA5: ContactData = {
+  label: 'E-mail',
+  value: ['newname@mail.ru'],
+};
+
+export const ONECARD1: OneCard = {
+  label: 'Для физических лиц',
+  data: [
+    [DATA1, DATA2],
+    [DATA1, DATA3],
+    [DATA4, DATA5],
+  ],
+};
+
+export const ONECARD2: OneCard = {
+  label: 'Для юридических лиц лиц',
+  data: [[DATA1, DATA2], [DATA3], [DATA4, DATA5]],
+};
+
+export const ONECARD3: OneCard = {
+  label: 'Хранилище ценностей',
+  data: [[DATA1, DATA2]],
+};
+
+export const ALLCARDS: ContactInformationContent = {
+  title: 'Контактная информация',
+  cards: [ONECARD1, ONECARD2, ONECARD3],
+};
+
+// export default {
+//   default: (
+//     <div className="container grid grid-cols-12">
+//       <ContactInformation className="col-span-12" context={context} {...ALLCARDS} />
+//     </div>
+//   ),
+// };

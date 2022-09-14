599339dba93a8e0ff259bfbc32f95e4014ce419c
AlexsaTasara <kirin1337@gmail.com>
2022-09-14 13:20:14 +0300

    draft Contact Information

--- src/components/ContactInformation/ContactInformation.fixture.tsx
+++ src/components/ContactInformation/ContactInformation.fixture.tsx
@@ -4,57 +4,57 @@ import type { ContactData, ContactInformationContent, OneCard } from './ContactI
 
 export const DATA1: ContactData = {
   label: 'Телефон филиала',
-  value: ['8(495)200-78-70'],
+  value: ['8 (495) 200-78-70'],
 };
 
 export const DATA2: ContactData = {
   label: 'Операционный зал',
-  value: ['8(495)200-78-70'],
+  value: ['8 (495) 200-78-70'],
 };
 
 export const DATA3: ContactData = {
   label: 'Для консультации по контролю валюты',
-  value: ['8(800)200-78-70', '8(495)773-20-19'],
+  value: ['8 (800) 200-78-70', '8 (495) 773-20-19'],
 };
 
 export const DATA4: ContactData = {
   label: 'Факс',
-  value: ['8(800)200-78-70'],
+  value: ['8 (800) 200-78-70'],
 };
 
 export const DATA5: ContactData = {
   label: 'E-mail',
   value: ['newname@mail.ru'],
+  textColor: 'green',
 };
 
 export const ONECARD1: OneCard = {
   label: 'Для физических лиц',
-  data: [
-    [DATA1, DATA2],
-    [DATA1, DATA3],
-    [DATA4, DATA5],
-  ],
+  data: [DATA1, DATA2, DATA1, DATA3, DATA4, DATA5],
+  icon: { icon: 'DocumentTextIcon' },
 };
 
 export const ONECARD2: OneCard = {
   label: 'Для юридических лиц лиц',
-  data: [[DATA1, DATA2], [DATA3], [DATA4, DATA5]],
+  data: [DATA1, DATA2, DATA3, DATA4, DATA5],
+  icon: { icon: 'DocumentTextIcon' },
 };
 
 export const ONECARD3: OneCard = {
   label: 'Хранилище ценностей',
-  data: [[DATA1, DATA2]],
+  data: [DATA1, DATA2],
+  icon: { icon: 'DocumentTextIcon' },
 };
 
 export const ALLCARDS: ContactInformationContent = {
   title: 'Контактная информация',
-  cards: [ONECARD1, ONECARD2, ONECARD3],
+  contactCards: [ONECARD1, ONECARD2, ONECARD3],
 };
 
-// export default {
-//   default: (
-//     <div className="container grid grid-cols-12">
-//       <ContactInformation className="col-span-12" context={context} {...ALLCARDS} />
-//     </div>
-//   ),
-// };
+export default {
+  default: (
+    <div className="container grid grid-cols-12">
+      <ContactInformation className="col-span-12" context={context} {...ALLCARDS} />
+    </div>
+  ),
+};
--- src/components/ContactInformation/ContactInformationContent.ts
+++ src/components/ContactInformation/ContactInformationContent.ts
@@ -1,17 +1,28 @@
 import { Picture } from '../../model/Picture';
 
+/**
+ * @title Цвет текста
+ * @enumNames [
+ *    "Красный",
+ *    "Зеленый",
+ *    "Черный"
+ * ]
+ */
+export type TextColorVersion = 'red' | 'green' | 'black';
+
 export interface ContactInformationContent {
   title?: string;
-  cards?: OneCard[];
+  contactCards?: OneCard[];
 }
 
 export interface OneCard {
   icon?: Picture;
   label?: string;
-  data?: ContactData[][];
+  data?: ContactData[];
 }
 
 export interface ContactData {
   label?: string;
   value?: string[];
+  textColor?: TextColorVersion;
 }

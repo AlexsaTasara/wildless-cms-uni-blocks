40f0f7ef4fd1487a1ca950f08e8f1f09e776799b
AlexsaTasara <kirin1337@gmail.com>
2022-09-14 15:02:32 +0300

    Contact Information

--- src/components/ContactInformation/ContactInformation.tsx
+++ src/components/ContactInformation/ContactInformation.tsx
@@ -1,8 +1,20 @@
 import { JSX } from '@redneckz/uni-jsx';
 import type { UniBlockProps } from '../../types';
 import { Heading } from '../../ui-kit/Heading/Heading';
+import { Img } from '../../ui-kit/Img/Img';
 import { getTileHeadingType } from '../BaseTile/getTileHeadingType';
-import type { ContactData, ContactInformationContent, OneCard } from './ContactInformationContent';
+import type {
+  ContactData,
+  ContactInformationContent,
+  OneCard,
+  TextColorVersion,
+} from './ContactInformationContent';
+
+const TEXT_COLORS: Record<TextColorVersion, string> = {
+  black: 'text-black-500',
+  red: 'text-red-500',
+  green: 'text-green-500',
+};
 
 export interface ContactInformationProps extends ContactInformationContent, UniBlockProps {}
 
@@ -13,52 +25,59 @@ export const ContactInformation = JSX<ContactInformationProps>((props) => {
       <section>
         {title ? (
           <Heading
-            className="w-full ml-5 mb-8"
+            className="w-full ml-10 mb-8"
             headingType={getTileHeadingType(className)}
             title={title}
           />
         ) : null}
       </section>
-      {contactCards ? <div className="flex flex-row">{contactCards.map(renderCard)}</div> : null}
+      {contactCards ? (
+        <div className="flex flex-row ml-8">{contactCards.map(renderCard)}</div>
+      ) : null}
     </section>
   );
 });
 
 function renderCard(card: OneCard, i: number) {
   return (
-    <div key={String(i)} className="flex flex-col mb-8">
-      <div className="flex gap-1 flex-row h-full ml-5 max-w-[490px]">
-        {card.label ? (
-          <div className="font-medium text-primary-text text-sm m-0">{card.label}</div>
+    <div key={String(i)} className="mb-8 ml-4">
+      <div className="flex gap-1 flex-col h-full max-w-[990px]">
+        <div className="flex flex-row mb-4">
+          {card.icon ? (
+            <Img
+              className="w-[48px] h-[48px] bg-main rounded-full box-border p-[12px]"
+              image={card.icon}
+              width="48"
+              height="48"
+              asSVG
+            />
+          ) : null}
+          {card.label ? (
+            <div className="font-medium text-primary-text text-md ml-2 p-[12px]">{card.label}</div>
+          ) : null}
+        </div>
+        {card.data ? (
+          <div className="grid grid-cols-2 gap-1">{card.data.map(renderData)}</div>
         ) : null}
-        {card.data ? <div className="flex">{card.data.map(renderDataCol)}</div> : null}
       </div>
     </div>
   );
 }
 
-function renderDataCol(data: ContactData[], i: number) {
+function renderData(data: ContactData, i: number) {
+  let textcol = TEXT_COLORS['green'];
+  data.textColor ? (textcol = data.textColor) : null;
   return (
-    <div key={String(i)} className="flex flex-row mb-8">
-      <div className="flex gap-1 flex-row h-full ml-5 max-w-[490px]">
-        {data ? (
-          <div className="font-medium text-primary-text text-sm m-0">{data.map(renderDataRow)}</div>
-        ) : null}
-      </div>
-    </div>
-  );
-}
-
-function renderDataRow(data: ContactData, i: number) {
-  return (
-    <div key={String(i)} className="flex flex-col mb-8">
-      <div className="flex gap-1 flex-row h-full ml-5 max-w-[490px]">
+    <div key={String(i)} className="mb-8">
+      <div className="gap-2">
         {data.label ? (
-          <div className="font-medium text-primary-text text-sm m-0">{data.label}</div>
+          <div className="font-normal text-secondary-text text-sm mb-2 w-[180px]">{data.label}</div>
         ) : null}
         {data.value ? (
-          <div className="font-medium text-primary-text text-sm m-0">
-            {data.value.map((val) => val)}
+          <div className={`font-medium text-primary-text text-md m-0 ${TEXT_COLORS[textcol]}`}>
+            {data.value.map((val) => (
+              <div>{val}</div>
+            ))}
           </div>
         ) : null}
       </div>
--- src/components/ContactInformation/ContactInformationContent.ts
+++ src/components/ContactInformation/ContactInformationContent.ts
@@ -1,5 +1,15 @@
 import { Picture } from '../../model/Picture';
 
+/**
+ * @title Цвет текста
+ * @enumNames [
+ *    "Красный",
+ *    "Зеленый",
+ *    "Черный"
+ * ]
+ */
+export type TextColorVersion = 'red' | 'green' | 'black';
+
 export interface ContactInformationContent {
   title?: string;
   contactCards?: OneCard[];
@@ -8,10 +18,11 @@ export interface ContactInformationContent {
 export interface OneCard {
   icon?: Picture;
   label?: string;
-  data?: ContactData[][];
+  data?: ContactData[];
 }
 
 export interface ContactData {
   label?: string;
   value?: string[];
+  textColor?: TextColorVersion;
 }

8255d8097e78a9a0f643e046056ddcce0da79f84
AlexsaTasara <kirin1337@gmail.com>
2022-10-10 12:18:32 +0300

    Office card resolve conversations

--- src/components/OfficeCard/OfficeCard.tsx
+++ src/components/OfficeCard/OfficeCard.tsx
@@ -1,7 +1,7 @@
 import { JSX } from '@redneckz/uni-jsx';
 import type { UniBlockProps } from '../../types';
 import { Heading } from '../../ui-kit/Heading/Heading';
-import { getTileHeadingType } from '../BaseTile/getTileHeadingType';
+
 import type {
   ContactInformation,
   Metro,
@@ -27,46 +27,34 @@ export const OfficeCard = JSX<OfficeCardProps>((props) => {
     contactsInfo,
     dates,
     currency,
-    anchor = null,
+    anchor = '',
   } = props;
 
   return (
     <section id={anchor} className={`font-sans text-primary-text py-10 bg-white ${className}`}>
-      {renderHead({ title, link, location, className })}
+      {renderHead(title, link, location)}
       {metros ? <div className="flex flex-row">{metros.map(renderMetro)}</div> : null}
       {contactsInfo ? (
         <div className="flex flex-row">{contactsInfo.map(renderContacts)}</div>
       ) : null}
       {dates ? <div className="flex flex-row">{dates.map(renderDates)}</div> : null}
-      {currency ? <div className="flex flex-row">{currency.map(renderCurrency)}</div> : null}
+      {currency?.length ? (
+        <div className="flex flex-row">{currency.map(renderCurrency)}</div>
+      ) : null}
     </section>
   );
 });
 
-function renderHead({ title, link, location, className }) {
+function renderHead(
+  title: string | undefined,
+  link: string | undefined,
+  location: string | undefined,
+) {
   return (
     <section>
-      {title ? (
-        <Heading
-          className="w-full ml-5 mb-8"
-          headingType={getTileHeadingType(className)}
-          title={title}
-        />
-      ) : null}
-      {link ? (
-        <Heading
-          className="w-full ml-5 mb-8"
-          headingType={getTileHeadingType(className)}
-          title={link}
-        />
-      ) : null}
-      {location ? (
-        <Heading
-          className="w-full ml-5 mb-8"
-          headingType={getTileHeadingType(className)}
-          title={location}
-        />
-      ) : null}
+      {title ? <Heading className="w-full ml-5 mb-8" headingType="h2" title={title} /> : null}
+      {link ? <Heading className="w-full ml-5 mb-8" headingType="h2" title={link} /> : null}
+      {location ? <Heading className="w-full ml-5 mb-8" headingType="h2" title={location} /> : null}
     </section>
   );
 }
@@ -75,7 +63,6 @@ function renderMetro(metro: Metro, i: number) {
   return (
     <div key={String(i)} className="flex flex-col mb-8">
       <div className="flex gap-1 flex-row h-full ml-5 max-w-[490px]">
-
         {metro?.name ? (
           <div className="font-medium text-primary-text text-sm m-0">{metro.name}</div>
         ) : null}
@@ -106,8 +93,8 @@ function renderContacts(contact: ContactInformation, i: number) {
 }
 
 function renderDates(curdate: Weekday, i: number) {
-  let textcol = TEXT_COLORS['green'];
-  curdate?.textColor ? (textcol = curdate.textColor) : null;
+  let textCol = TEXT_COLORS['green'];
+  curdate?.textColor ? (textCol = curdate.textColor) : null;
 
   return (
     <div key={String(i)} className="flex flex-row mb-8">
@@ -119,7 +106,7 @@ function renderDates(curdate: Weekday, i: number) {
           <div className="font-medium text-primary-text text-xl m-0">{curdate.time}</div>
         ) : null}
         {curdate?.status ? (
-          <div className={`font-normal text-sm text-secondary-text ${TEXT_COLORS[textcol]}`}>
+          <div className={`font-normal text-sm text-secondary-text ${TEXT_COLORS[textCol]}`}>
             {curdate.status}
           </div>
         ) : null}

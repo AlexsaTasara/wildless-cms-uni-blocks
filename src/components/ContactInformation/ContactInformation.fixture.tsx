beaddec0641b6e43b1424eb464aebfe9e5c6bcbf
AlexsaTasara <kirin1337@gmail.com>
2022-11-09 10:03:06 +0300

    Office card and Contact Information

--- src/components/ContactInformation/ContactInformation.fixture.tsx
+++ src/components/ContactInformation/ContactInformation.fixture.tsx
@@ -6,50 +6,50 @@ import type {
   ContactInformationContent,
 } from './ContactInformationContent';
 
-export const DATA1: ContactData = {
+const DATA1: ContactData = {
   label: 'Телефон филиала',
   values: ['8 (495) 200-78-70', '8 (195) 860-28-71'],
 };
 
-export const DATA2: ContactData = {
+const DATA2: ContactData = {
   label: 'Операционный зал',
   values: ['8 (495) 200-78-70'],
 };
 
-export const DATA3: ContactData = {
+const DATA3: ContactData = {
   label: 'Для консультации по контролю валюты',
   values: ['8 (800) 200-78-70', '8 (495) 773-20-19'],
 };
 
-export const DATA4: ContactData = {
+const DATA4: ContactData = {
   label: 'Факс',
   values: ['8 (800) 200-78-70'],
 };
 
-export const DATA5: ContactData = {
+const DATA5: ContactData = {
   label: 'E-mail',
   values: ['newname@mail.ru'],
 };
 
-export const ONECARD1: ContactCard = {
+const ONECARD1: ContactCard = {
   label: 'Для физических лиц',
   data: [DATA1, DATA2, DATA1, DATA3, DATA4, DATA5],
   icon: { icon: 'DocumentTextIcon' },
 };
 
-export const ONECARD2: ContactCard = {
+const ONECARD2: ContactCard = {
   label: 'Для юридических лиц лиц',
   data: [DATA1, DATA2, DATA3, DATA4, DATA5],
   icon: { icon: 'DocumentTextIcon' },
 };
 
-export const ONECARD3: ContactCard = {
+const ONECARD3: ContactCard = {
   label: 'Хранилище ценностей',
   data: [DATA1, DATA2],
   icon: { icon: 'DocumentTextIcon' },
 };
 
-export const ALLCARDS: ContactInformationContent = {
+const ALLCARDS: ContactInformationContent = {
   title: 'Контактная информация',
   contactCards: [ONECARD1, ONECARD2, ONECARD3],
 };
